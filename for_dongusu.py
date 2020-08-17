f45da2784b409fbc968bcb4b3d70552b845cd8e0
Enes <69701196+enesaygur@users.noreply.github.com>
2020-08-18 00:07:35 +0300

    Add files via upload

--- for_dongusu.py
+++ for_dongusu.py
@@ -13,8 +13,36 @@ sayilar=[1,3,5,7,9,12,19,21]
 # print(top)  
 #
 # sayilar listesindeki tek sayıların karesini alınız? ####################
-for say in sayilar:
-    if (say%2)!=0:
-        print(say," tek olduğu için sayının karesi",say*say)
-    else:
-        print(f"{say} çifttir ")
+# for say in sayilar:
+#     if (say%2)!=0:
+#         print(say," tek olduğu için sayının karesi",say*say)
+#     else:
+#         print(f"{say} çifttir ")
+##############################################################################
+sehirler=['kocaeli','istanbul','ankara','izmir','rize']
+# Şehirlerden hangisi en fazla 5 karakterlidir?
+# for sehir in sehirler:
+#     if len(sehir)<=5:
+#         print(sehir)
+#     else:
+#         print(f"{sehir} -Karakter sayısı 5 ten fazladır ")
+#
+urunler = [
+{'name':'samsung s6','price':'3000'},
+{'name':'samsung s7','price':'4000'},
+{'name':'samsung s8','price':'5000'},
+{'name':'samsung s9','price':'6000'},
+{'name':'samsung s10','price':'7000'}
+          ]
+# toplam=0
+# for urun in urunler:
+#     fiyat=int(urun['price'])
+#     print(fiyat)
+#     toplam+=fiyat
+# print("toplam ürün fiyatı =",toplam)
+
+# fiyatı 5000 ve altı olan ürünler hangileridir?
+
+for urun in urunler:
+    if int(urun['price'])<5000:
+        print(f"{urun['name']} fiyatı {urun['price']}")
